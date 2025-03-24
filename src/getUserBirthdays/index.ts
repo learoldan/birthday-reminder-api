1a0d23d4ced5dca7351999459e204e9d19ec1467
leandro <learoldan.dev@gmail.com>
2025-03-24 19:53:46 +0100

    added eslint

--- src/getUserBirthdays/index.ts
+++ src/getUserBirthdays/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export async function handler(event: any) {
     console.log('event:', event)
     return {
--- src/updateBirthday/index.ts
+++ src/updateBirthday/index.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function handler(event: any) {
     console.log('event:', event)
     return {
