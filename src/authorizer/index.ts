77c76efe56cbc658ba9aa22ea597de6d3a088b69
leandro <learoldan.dev@gmail.com>
2025-03-26 13:43:40 +0100

    refactor: changed serverless structure and config with independents lambda yml files

--- src/authorizer/index.ts
+++ src/authorizer/index.ts
@@ -3,34 +3,8 @@ import {
     APIGatewayTokenAuthorizerEvent,
     APIGatewayAuthorizerResult,
 } from 'aws-lambda'
-import jwt, { VerifyErrors, JwtHeader, SigningKeyCallback } from 'jsonwebtoken'
-import jwksClient from 'jwks-rsa'
-// import { getKey } from '../../lib/jwt'
-
-// Configuración del cliente JWKS
-const client = jwksClient({
-    jwksUri: process.env.AUTH0_JWKS_URI as string,
-})
-
-// Función corregida
-export const getKey = (
-    header: JwtHeader,
-    callback: SigningKeyCallback
-): void => {
-    if (!header.kid) {
-        callback(new Error('No KID found in token header'), undefined)
-        return
-    }
-
-    client.getSigningKey(header.kid, (err, key) => {
-        if (err) {
-            callback(err, undefined)
-        } else {
-            const signingKey = key?.getPublicKey()
-            callback(null, signingKey)
-        }
-    })
-}
+import jwt, { VerifyErrors } from 'jsonwebtoken'
+import { getKey } from './jwt'
 
 export const handler = async (
     event: APIGatewayTokenAuthorizerEvent
